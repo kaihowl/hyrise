29a2c38a05a97923f1631a406948a43a9be72639
kaihowl <kai@hoewelmeyer.eu>
2014-05-23 08:35:19 +0000

    Inner-loop idea programmed

--- src/lib/access/DoublePipelinedHashJoin.cpp
+++ src/lib/access/DoublePipelinedHashJoin.cpp
@@ -1,5 +1,6 @@
 #include "access/DoublePipelinedHashJoin.h"
 #include "storage/meta_storage.h"
+#include <algorithm>
 
 namespace hyrise {
 namespace access {
@@ -24,16 +25,35 @@ void DoublePipelinedHashJoin::executePlanOperation() {
   // TODO maybe only set the required field on the index upon copy
   field_t f = _indexed_field_definition[_source_task_index];
 
+  storage::pos_list_t this_rows, other_rows;
+
   for (pos_t row = 0; row < input->size(); ++row) {
     //insert into hashtable
     row_hash_functor<join_key_t> fun(input.get(), f, row);
     storage::type_switch<hyrise_basic_types> ts;
     join_key_t hash = ts(input->typeOfColumn(f), fun);
-    // TODO insert the source table as well!!!
-    _hashtable->insert(hashtable_t::value_type(hash, std::pair<taskscheduler::task_ptr_t, pos_t>(_source_task, row)));
-  //iterator over rows.
-  //get matching_rows
-  //filter by table
+    join_value_t val = std::pair<taskscheduler::task_ptr_t, pos_t>(_source_task, row);
+    hashtable_t::value_type insert_key(hash, val);
+    _hashtable->insert(insert_key);
+
+    // TODO maybe reserve full size before.
+    hashtable_t::iterator all_matches_start, all_matches_end;
+    std::tie(all_matches_start, all_matches_end) = _hashtable->equal_range(hash);
+    // find our end
+    auto matches_end = std::find(all_matches_end, all_matches_end, insert_key);
+    // TODO maybe reserve full size before.
+    // TODO and use standard iterator again
+    std::vector<hashtable_t::value_type> matching_keys;
+    pos_list_t matching_rows;
+    // only use matching rows not in our table
+    std::remove_copy_if(all_matches_start, matches_end, back_inserter(matching_keys), [this] (const hashtable_t::value_type& val) { return val.second.first == _source_task; });
+    std::transform(matching_keys.begin(), matching_keys.end(), back_inserter(matching_rows), [](const hashtable_t::value_type& val) {return val.second.second;});
+
+    if (!matching_rows.empty()) {
+      this_rows.insert(this_rows.end(), matching_rows.size(), row);
+      other_rows.insert(other_rows.end(), matching_rows.begin(), matching_rows.end());
+    }
+  }
   //construct output table
   //TODO later emit chunks of required chunk size
   }
