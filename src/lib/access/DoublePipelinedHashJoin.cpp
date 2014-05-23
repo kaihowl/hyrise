b592646f503675c40b27e8373011bfe68fd6aef0
kaihowl <kai@hoewelmeyer.eu>
2014-05-23 12:42:04 +0000

    Store table chunks
    Since we do not have the "other" table in chunk, we need to ensure that
    matching positions indicate the table chunk from which they stem. These table
    chunks need to be constructed again for the reconstruction of the table at the
    end.

--- src/lib/access/DoublePipelinedHashJoin.cpp
+++ src/lib/access/DoublePipelinedHashJoin.cpp
@@ -1,5 +1,9 @@
 #include "access/DoublePipelinedHashJoin.h"
+
 #include "storage/meta_storage.h"
+#include "storage/TableRangeView.h"
+#include "storage/PointerCalculator.h"
+
 #include <algorithm>
 
 namespace hyrise {
@@ -7,10 +11,12 @@ namespace access {
 
 DoublePipelinedHashJoin::DoublePipelinedHashJoin() : PlanOperation() {
   _hashtable = std::make_shared<hashtable_t>();
+  _chunk_tables = std::make_shared<tbb::concurrent_vector<storage::c_atable_ptr_t>>();
 }
 
 DoublePipelinedHashJoin::DoublePipelinedHashJoin(const DoublePipelinedHashJoin& original) : PlanOperation() {
   _hashtable = original._hashtable;
+  _chunk_tables = original._chunk_tables;
 }
 
 void DoublePipelinedHashJoin::executePlanOperation() {
@@ -18,6 +24,8 @@ void DoublePipelinedHashJoin::executePlanOperation() {
   if (!input) {
     return;
   }
+
+  _chunk_tables->push_back(input);
 
   // determine index of source op
   // TODO maybe only set the required field on the index upon copy
@@ -30,31 +38,54 @@ void DoublePipelinedHashJoin::executePlanOperation() {
     row_hash_functor<join_key_t> fun(input.get(), f, row);
     storage::type_switch<hyrise_basic_types> ts;
     join_key_t hash = ts(input->typeOfColumn(f), fun);
-    join_value_t val = std::pair<taskscheduler::task_ptr_t, pos_t>(_source_task, row);
+    // Store absolute positions
+    join_value_t val = std::make_tuple(_source_task.get(), input.get(), row);
     hashtable_t::value_type insert_key(hash, val);
     _hashtable->insert(insert_key);
+
+    // TODO optimize if the other table is not yet available.
 
     // TODO maybe reserve full size before.
     hashtable_t::iterator all_matches_start, all_matches_end;
     std::tie(all_matches_start, all_matches_end) = _hashtable->equal_range(hash);
     // find our end
     auto matches_end = std::find(all_matches_end, all_matches_end, insert_key);
+
     // TODO maybe reserve full size before.
     // TODO and use standard iterator again
-    std::vector<hashtable_t::value_type> matching_keys;
-    pos_list_t matching_rows;
-    // only use matching rows not in our table
-    std::remove_copy_if(all_matches_start, matches_end, back_inserter(matching_keys), [this] (const hashtable_t::value_type& val) { return val.second.first == _source_task; });
-    std::transform(matching_keys.begin(), matching_keys.end(), back_inserter(matching_rows), [](const hashtable_t::value_type& val) {return val.second.second;});
 
-    if (!matching_rows.empty()) {
-      this_rows.insert(this_rows.end(), matching_rows.size(), row);
-      other_rows.insert(other_rows.end(), matching_rows.begin(), matching_rows.end());
-    }
+    std::vector<hashtable_t::value_type> matching_keys;
+    // only use matching rows not in our table
+    std::remove_copy_if(all_matches_start, matches_end, back_inserter(matching_keys), [this] (const hashtable_t::value_type& val) { return std::get<0>(val.second) == _source_task.get(); });
+
+    std::vector<std::pair<const storage::AbstractTable*, storage::pos_t>> matching_tables_rows;
+    std::transform(matching_keys.begin(), matching_keys.end(), back_inserter(matching_tables_rows), [](const hashtable_t::value_type& val) {return std::pair<const storage::AbstractTable*, storage::pos_t>(std::get<1>(val.second), std::get<2>(val.second));});
+
+    //if (!matching_rows.empty()) {
+      //this_rows.insert(this_rows.end(), matching_rows.size(), row);
+      //other_rows.insert(other_rows.end(), matching_rows.begin(), matching_rows.end());
+    //}
   }
-  //construct output table
+
+  //if ((*_source_tables)[0] && (*_source_tables)[1]) {
+    //std::vector<storage::atable_ptr_t> parts;
+
+    //auto this_table_pc = storage::PointerCalculator::create((*_source_tables)[_source_task_index]);
+    //// TODO this can be handled nicer, ey?
+    //auto other_index = _source_task_index == 0 ? 1 : 0;
+    //auto other_table_pc = storage::PointerCalculator::create((*_source_tables)[other_index]);
+
+    //parts.push_back(this_table_pc);
+    //parts.push_back(other_table_pc);
+
+    //storage::atable_ptr_t result = std::make_shared<storage::MutableVerticalTable>(parts);
+    //addResult(result);
+  //}
+
   //TODO later emit chunks of required chunk size
-  }
+  //
+  // emits nothing if not both source tables had been available
+
 }
 
 std::shared_ptr<PlanOperation> DoublePipelinedHashJoin::parse(const Json::Value& data) {
--- src/lib/access/DoublePipelinedHashJoin.h
+++ src/lib/access/DoublePipelinedHashJoin.h
@@ -5,6 +5,7 @@
 #include "access/system/PlanOperation.h"
 
 #include "tbb/concurrent_unordered_map.h"
+#include "tbb/concurrent_vector.h"
 
 #include "storage/storage_types.h"
 #include "storage/meta_storage.h"
@@ -50,7 +51,7 @@ class DoublePipelinedHashJoin : public PlanOperation, public PipelineObserver<Do
  private:
   // TODO maybe we should use the same hasher as in other multimap here
   typedef size_t join_key_t ;
-  typedef std::pair<taskscheduler::task_ptr_t, pos_t> join_value_t;
+  typedef std::tuple<taskscheduler::Task*, const storage::AbstractTable*, storage::pos_t> join_value_t;
   typedef tbb::concurrent_unordered_multimap<join_key_t, join_value_t> hashtable_t;
   typedef std::shared_ptr<hashtable_t> hashtable_ptr_t;
 
@@ -58,6 +59,10 @@ class DoublePipelinedHashJoin : public PlanOperation, public PipelineObserver<Do
   // all values will be hashed before. This is a hacky way of "casting" them to
   // a common datatype. (This is also in place for the storage::HashTable.)
   hashtable_ptr_t _hashtable;
+
+  // this is just there to manage the lifetime of chunks
+  // it enables us to safely store references to shared_ptr in the hashtable
+  std::shared_ptr<tbb::concurrent_vector<storage::c_atable_ptr_t>> _chunk_tables;
 };
 }
 }
